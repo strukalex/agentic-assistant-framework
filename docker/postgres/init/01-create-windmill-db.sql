105f8abb075c1be851b5231c7afb49ab7f95a115
alex <strukalex@gmail.com>
2025-12-24 12:33:46 -0800

    implementation fix - docker compose db init

---
+++ docker/postgres/init/01-create-windmill-db.sql
@@ -0,0 +1,6 @@
+-- Create windmill database for Windmill orchestration service
+-- This script runs automatically when the postgres container is first initialized
+-- (only runs if the data directory is empty)
+
+CREATE DATABASE windmill;
+
